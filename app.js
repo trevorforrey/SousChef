231ae57f1eb5243c08c796f5551aaed1c7548088
Trevor Forrey <trevor4e@gmail.com>
2018-09-18 19:29:08 +0000

    updated port to match nginx on server

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
+app.listen(5000, function () {
   console.log('Example app listening on port 3000!');
 });
