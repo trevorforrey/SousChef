d9279698d110cb523f0a6b83eb4e771647accc52
Trevor Forrey <trevor4e@gmail.com>
2018-09-28 14:32:26 -0700

    heroku release debugging

--- release-tasks.sh
+++ release-tasks.sh
@@ -9,3 +9,8 @@ touch key.json
 echo $privKey >> key.json
 
 export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)+"/key.json"
+
+echo "File Contents:"
+echo $privKey
+echo "File Path: "
+echo ${GOOGLE_APPLICATION_CREDENTIALS}
