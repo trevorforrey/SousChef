99cd95eb879e1df7629c3f2f6ddbaf8f01308c99
Trevor Forrey <trevor4e@gmail.com>
2018-09-30 12:02:56 -0700

    Updated Prep and Cook time to use Mongo helper function

--- cooktime_intent.js
+++ cooktime_intent.js
@@ -1,43 +1,16 @@
-let MongoClient = require('mongodb').MongoClient;
+import get_recipe from './mongo_helper'
 
 async function getCookTime() {
-    let client;
-    let mongo_pw = process.env.MONGO_PW;
-    let uri = "mongodb+srv://tforrey:" + mongo_pw + "@cluster0-mypdv.mongodb.net/test?retryWrites=true";
-    let cook_time = null;
+    let recipe_doc = await get_recipe("Todd's Favorite Blueberry Pancakes");
     
-    try {
-        client =await MongoClient.connect(uri);
-        console.log("Connected correctly to the server");
-        
-        const db = client.db('sous-chef');
-        
-        // Get the recipes collection
-        const collection = db.collection('recipes');
-        
-        // Find recipe document for Blueberry Pancakes
-        const cursor = await collection.find({name : "Todd's Favorite Blueberry Pancakes"}).limit(1);
-        
-        // Get recipe document from cursor
-        const recipe_doc = await cursor.next();
-        
-        // Get the cook time field from the recipe
-        let cook_time = recipe_doc.cook_time;
-        
-    } catch (err) {
-        console.log(err.stack);
-        client.close();
-    }
-    
-    // Close the connection before returning cook time
-    client.close();
-    console.log('cook time connection has closed');
+    // Get the cook time field from the recipe
+    let cook_time = recipe_doc.cook_time;
    
     return cook_time;
 }
 
 // allows us to import the function in app.js
-export default getCookTime();
+export default getCookTime;
 
 
     
--- prep-time_intent.js
+++ prep-time_intent.js
@@ -1,34 +1,19 @@
-let MongoClient = require('mongodb').MongoClient;
+import get_recipe from './mongo_helper'
 
 /*
     Returns the amount of time it will take to finish the client's recipe.
 */
 
 async function getPrepTime(){
-    let client;
-    let prepTime = "";
-    let pw = process.env.MONGO_PW;
-    let uri = "mongodb+srv://tforrey:" + mongo_pw + "@cluster0-mypdv.mongodb.net/test?retryWrites=true";
-    try {
-        //connect to database and retrieve recipe
-        client = await MongoClient.connect(uri);
-        const dbo = client.db('sous-chef');
-        const recipes = dbo.collection('recipes');
-        //This step will need to be updated in phase 2 to retrieve any recipe
-        let recipe = await recipes.findOne({name : "Todd's Favorite Blueberry Pancakes"}, {prep_time: 1});
-        if (recipe == null){
-            prepTime = null;
-        } else {
-            prepTime = recipe.prep_time
-        }
-    } catch (err) {
-        console.log(err.stack);
-        client.close();
+    let recipe_doc = await get_recipe("Todd's Favorite Blueberry Pancakes");
+
+    if (recipe_doc == null){
+        prepTime = null;
+    } else {
+        prepTime = recipe_doc.prep_time
     }
-    
-    // Close connection before returning
-    client.close();
-    console.log('connection closed');
 
     return prepTime;
 }
+
+export default getPrepTime;
