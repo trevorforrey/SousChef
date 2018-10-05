e38885f30e8e9cbc0a1d040b678cf935a163fa3b
Trevor Forrey <trevor4e@gmail.com>
2018-10-05 10:24:14 -0700

    Added Steps tests and another ingredient amount test

--- test/ingredientAmount_test.js
+++ test/ingredientAmount_test.js
@@ -102,6 +102,51 @@ const ingredientAmountRequestFlour = {
     "session": "projects/testagent-be9db/agent/sessions/ca8047bf-9fb2-3a6b-88b0-f610254dc8b2"
 };
 
+const ingredientAmountRequestBlueberries = {
+    "responseId": "a631f795-526a-45ee-99f3-97c33f596296",
+    "queryResult": {
+        "queryText": "How many blueberries do I need?",
+        "parameters": {
+        "ingredient": "blueberries"
+        },
+        "allRequiredParamsPresent": true,
+        "fulfillmentText": "You need 4 lbs of $any.",
+        "fulfillmentMessages": [
+        {
+            "text": {
+            "text": [
+                "You need 4 lbs of $any."
+            ]
+            }
+        }
+        ],
+        "outputContexts": [
+        {
+            "name": "projects/testagent-be9db/agent/sessions/ca8047bf-9fb2-3a6b-88b0-f610254dc8b2/contexts/ingredient-amount",
+            "lifespanCount": 5,
+            "parameters": {
+            "ingredient": "sugar",
+            "unit-weight-name.original": "",
+            "unit-weight-name": "",
+            "unit-volume-name.original": "",
+            "unit-volume-name": "",
+            "ingredient.original": "sugar"
+            }
+        }
+        ],
+        "intent": {
+        "name": "projects/testagent-be9db/agent/intents/651f9aa1-7cfc-49f9-b6f8-5679942c4d48",
+        "displayName": "Ingredient-Intent-Follow-Up"
+        },
+        "intentDetectionConfidence": 1,
+        "languageCode": "en"
+    },
+    "originalDetectIntentRequest": {
+        "payload": {}
+    },
+    "session": "projects/testagent-be9db/agent/sessions/ca8047bf-9fb2-3a6b-88b0-f610254dc8b2"
+};
+
 
 /************* TESTING BEGINS ***************/
 
@@ -144,6 +189,26 @@ describe('Test Getting amount of flour', () => {
                 response.should.have.status(201);
                 console.log('Fulfillment Text: ' + response.body.fulfillmentText);
                 response.body.fulfillmentText.should.include('flour');
+                done();
+            }
+        });
+    });
+});
+
+// Test getting amount of blueberries
+describe('Test Getting amount of blueberries', () => {
+    it('it should get amount of blueberries back', (done) => {
+    chai.request(app)
+        .post('/fulfillment')
+        .set('content-type', 'application/json')
+        .send(ingredientAmountRequestBlueberries)
+        .end( (error, response, body) => {
+            if (error) {
+                done(error);
+            } else {
+                response.should.have.status(201);
+                console.log('Fulfillment Text: ' + response.body.fulfillmentText);
+                response.body.fulfillmentText.should.include('blueberries');
                 done();
             }
         });
