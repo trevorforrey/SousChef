d32e13e3a2db100b80143d70e7edff229f59e6a5
Unknown <swdunnin@asu.edu>
2018-10-10 23:33:54 -0700

    US-79 & Task-101 Cook time test
    Create an automated test for the cook time intent for User story #79 and Task #101

---
+++ test/cookTime_test.js
@@ -0,0 +1,76 @@
+process.env.NODE_ENV = 'test';
+
+import 'babel-polyfill'
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let app = require('../src/app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+
+//Create json example POST body for testing
+const cookTimeRequest = {
+    "responseId": "6d38b2d9-f605-40fa-8930-a99107e8c472",
+    "queryResult": {
+        "queryText": "What is the cook time?",
+        "parameters": {
+            "recipe_indicator": "",
+            "Cook-Time-Entity": "cook time"
+        },
+        "allRequiredParamsPresent": true,
+        "fulfillmentText": "Your pancakes will take 15 minutes to cook.",
+        "fulfillmentMessages": [
+            {
+                "text": {
+                    "text": [
+                        "Your pancakes will take 15 minutes to cook."
+                    ]
+                }
+            }
+        ],
+        "intent": {
+            "name": "projects/testagent-be9db/agent/intents/c7af74ff-b6d1-4a0f-9d1d-a2edf70c8d70",
+            "displayName": "Cook-Time-Intent"
+        },
+        "intentDetectionConfidence": 0.77,
+        "languageCode": "en"
+    },
+    "originalDetectIntentRequest": {
+        "payload": {}
+    },
+    "session": "projects/testagent-be9db/agent/sessions/12fcb68c-9097-7fc5-450b-3fb341e32ddb"
+};
+
+
+
+/************* TESTING BEGINS ***************/
+
+
+// Describes All Tests in this file
+describe('Testing the Cook Time Intent', () => {
+
+
+// Test Cook Time Intent
+    describe('Test Cook Time Intent', () => {
+        it('it should get a response with the cook time of the recipe', (done) => {
+            chai.request(app)
+                .post('/fulfillment')
+                .set('content-type', 'application/json')
+                .send(cookTimeRequest)
+                .end( (error, response, body) => {
+                    if (error) {
+                        done(error);
+                    } else {
+                        response.should.have.status(201);
+                        console.log('Fulfillment Text: ' + response.body.fulfillmentText);
+                        response.body.fulfillmentText.should.include('For this recipe,');
+                        done();
+                    }
+                });
+        });
+    });
+    
+});
