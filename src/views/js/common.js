84e604211e445272f2b4cd6d6b3183de2c256ecc
Sumanth Paranjape <sparanj2@asu.edu>
2018-11-27 02:02:41 -0700

    Few changes to image file

--- src/views/js/common.js
+++ src/views/js/common.js
@@ -5,9 +5,9 @@ function popUpMessage(divID,message,isError,id){
     var modelPopUpContentCss = "<p class=\"modal-content\" style=\"text-align: center;width: 297px;padding-top: -10px;color: #eee;font-size: 14px;border-bottom-width: 20px;margin-bottom: 0px;padding-top: 0px;\"> <img src=\"";
     var image;
     if(isError){
-        image = "../img/error.png\">";
+        image = "../img/error.png\">&nbsp;";
     }else{
-       image =  "../img/green_tick.png\">";
+       image =  "../img/green_tick.png\">&nbsp;";
     }
     document.getElementById(divID).innerHTML = modelPopUpContentCss + image + message + "</p>";
     
